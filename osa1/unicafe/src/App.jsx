08e5a9ba7ebc7527130958ebb27d166ee9f48162
jansromi <jansromi@student.jyu.fi>
2023-10-26 12:29:11 +0300

    teht 1.7, refaktorointi olioksi

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
 
+/**
+ * H1 header component
+ * @param {string} text to display
+ * @returns header element
+ */
 const Header = ({ text }) => <h1>{text}</h1>
 
+/**
+ * @param {function} handleClick function to call when button is clicked
+ * @param {string} text to display on button 
+ * @returns button element
+ */
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-
+/**
+ * @param {string} text to display
+ * @param {number} rating to display
+ * @returns div element
+ */
 const Label = ({text, rating}) => {
   return (
     <div>
@@ -16,21 +30,62 @@ const Label = ({text, rating}) => {
 }
 
 const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  // refactor button states to an object
+  const [feedback, setFeedback] = useState({
+    ratings: {
+      good: 0,
+      neutral: 0,
+      bad: 0
+    },
+    // increment-method for feedbacks:
+    // increment: function(type) == create a function that takes a type as a parameter,
+    // representing 'good', 'neutral', 'bad' etc. feedback.
+    increment: function(type) {
+      setFeedback(prevState => ({
+        // object spread syntax, copies all properties from prevState to a new object
+        ...prevState,
+        ratings: {
+          ...prevState.ratings,
+          [type]: prevState.ratings[type] + 1
+        }
+      }));
+    },
+    // iterate over ratings and return the sum of all values
+    total: function() {
+      return Object.values(this.ratings).reduce((acc, currentValue) => acc + currentValue, 0);
+    },
+    // calculate the average of all ratings,
+    // good = 1, neutral = 0, bad = -1
+    average: function() {
+      const { good, neutral, bad } = this.ratings;
+      if (good + neutral + bad === 0) {
+        return 0;
+      }
+      return (good - bad) / (good + neutral + bad);
+    },
+    // calculate the percentage of good ratings
+    goodPercentage: function() {
+      const { good, neutral, bad } = this.ratings;
+      if (good + neutral + bad === 0) {
+        return (0) + "%";
+      }
+      return (good / (good + neutral + bad) * 100) + "%";
+    }
+  });
 
   return (
     <div>
       <Header text='Give feedback' />
-      <Button handleClick={() => setGood(good + 1)} text='Good' />
-      <Button handleClick={() => setNeutral(neutral + 1)} text='Neutral' />
-      <Button handleClick={() => setBad(bad + 1)} text='Bad' />
+      <Button handleClick={() => feedback.increment('good')} text='Good' />
+      <Button handleClick={() => feedback.increment('neutral')} text='Neutral' />
+      <Button handleClick={() => feedback.increment('bad')} text='Bad' />
       <Header text='Statistics' />
-      <Label text='Good' rating={good} />
-      <Label text='Neutral' rating={neutral} />
-      <Label text='Bad' rating={bad} />
+      <Label text='Good' rating={feedback.ratings.good} />
+      <Label text='Neutral' rating={feedback.ratings.neutral} />
+      <Label text='Bad' rating={feedback.ratings.bad} />
+      <Label text='All' rating={feedback.total()} />
+      <Label text='Average' rating={feedback.average()} />
+      <Label text='Positive' rating={feedback.goodPercentage()} />
     </div>
   )
 }
