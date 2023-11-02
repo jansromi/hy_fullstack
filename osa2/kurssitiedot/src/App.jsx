08a325bf54ac04c49fc394ef712b87ebd044afea
jansromi <rrobertjansson@gmail.com>
2023-11-02 18:59:18 +0200

    teht 2.5

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,42 +1,4 @@
-const Header = (props) => {
-  return ( 
-    <h1>{props.coursename}</h1>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      {props.osat.map(osa => (
-        <Part key={osa.name} part={osa.name} exercises={osa.exercises} />
-      ))}
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>{props.part} {props.exercises}</p>
-  )
-}
-
-const Total = (props) => {
-  let summa = props.osat.reduce((sum, osa) => sum + osa.exercises, 0);
-
-  return (
-    <b>Number of exercises {summa}</b>
-  )
-}
-
-const Course = (props) => {
-  return (
-    <div>
-      <Header coursename={props.course.name} />
-      <Content osat={props.course.parts} />
-      <Total osat={props.course.parts} />
-    </div>
-  )
-}
+import Course from "./Course"
 
 const Courses = (props) => {
   return (
